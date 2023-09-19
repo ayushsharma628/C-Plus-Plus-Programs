0adabe0eec4addd5d2fa6e81b2902dfe39ca2177
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-19 23:25:19 +0530

    Sum Of n Natural Numbers
    Sum Of n Natural Numbers Using Natural Number Formula

---
+++ 03_sumOfNaturalNo/03_sumOfNaturalNo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter The n Numbers: ";
+    cin>>n;
+    cout<<"Sum Of n Natural Numbers: "<<(n*(n+1))/2;
+
+    return 0;
+}
