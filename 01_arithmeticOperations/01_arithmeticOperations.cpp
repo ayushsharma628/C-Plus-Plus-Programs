200b6b75a2383693e3ff6ef15a862769ac149bc2
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-19 23:22:30 +0530

    Arithmetic Operations
    Addition, Subtraction, Multiplication, Division, Remainder

---
+++ 01_arithmeticOperations/01_arithmeticOperations.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter The First Number: ";
+    cin>>a;
+    cout<<"Enter The Second Number: ";
+    cin>>b;
+    cout<<"Addition: "<<a+b<<endl;
+    cout<<"Subtraction: "<<a-b<<endl;
+    cout<<"Multiplication: "<<a*b<<endl;
+    cout<<"Division: "<<a/b<<endl;
+    cout<<"Module: "<<a%b<<endl;
+
+    return 0;
+}
