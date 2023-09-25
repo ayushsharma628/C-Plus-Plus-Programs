0ec57721783077ab8b110f5df18515765b7f7a34
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-25 22:27:54 +0530

    Pattern Print Star Odd Middle
       *
      ***
     *****
    *******

---
+++ 26_patternPrintStarOddMiddle/26_patternPrintStarOddMiddle.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int total_rows;
+
+    cout<<"Enter The Rows: ";
+    cin>>total_rows;
+
+    for (int rows = 1; rows <=total_rows; rows++)
+    {
+        for (int cols=1;cols<=total_rows-rows; cols++)
+        {
+            cout<<" ";
+        }
+        for (int cols = 1; cols<=2*rows-1; cols++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+    
+    return 0;
+}
