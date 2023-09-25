16185f17aa205624b43affff539f588bcc559d9b
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-25 22:47:37 +0530

    Pattern Print Even Middle Op
    * * * * *
     * * * *
      * * *
       * *
        *

---
+++ 28_patternPrintStarEvenMiddleOp/28_patternPrintStarEvenMiddleOp.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int total_rows;
+
+    cout<<"Enter The Rows: ";
+    cin>>total_rows;
+
+    for (int rows = 1; rows <=total_rows; rows++)
+    {
+        for (int cols=1;cols<=rows-1; cols++)
+        {
+            cout<<" ";
+        }
+        for (int cols = rows; cols<=total_rows; cols++)
+        {
+            cout<<"*"<<" ";
+        }
+        
+        cout<<endl;
+    }
+    
+    return 0;
+}
