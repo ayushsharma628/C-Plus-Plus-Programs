88581ea303eb8bb05e2a1b2f3d3f2f33b2bf3f10
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-26 22:30:24 +0530

    Pattern Print Sand Clock
    * * * * *
     * * * *
      * * *
       * *
        *
        *
       * *
      * * *
     * * * *
    * * * * *

---
+++ 32_patternPrintSandClock/32_patternPrintSandClock.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int total_rows;
+
+    cout<<"Enter The Rows: ";
+    cin>>total_rows;
+
+    for (int rows = 1; rows <=total_rows; rows++)
+    {
+        for (int cols=1;cols<=rows-1; cols++)
+        {
+            cout<<" ";
+        }
+        for (int cols = rows; cols<=total_rows; cols++)
+        {
+            cout<<"*"<<" ";
+        }
+        
+        cout<<endl;
+    }
+    for (int rows = 1; rows <=total_rows; rows++)
+    {
+        for (int cols=1;cols<=total_rows-rows; cols++)
+        {
+            cout<<" ";
+        }
+        for (int cols = 1; cols<=rows; cols++)
+        {
+            cout<<"*"<<" ";
+        }
+        
+        cout<<endl;
+    }
+    
+    return 0;
+}
