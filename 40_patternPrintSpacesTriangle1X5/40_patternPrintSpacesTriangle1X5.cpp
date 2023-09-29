6dd0e7b94bf3fa44932d82c9d20dc0c54bf20d7d
ayushsharma628 <ayushsharma6283@outlook.com>
2023-09-29 22:56:33 +0530

    Pattern Print Spaces Triangle 1X5
        1
       21
      321
     4321
    54321

---
+++ 40_patternPrintSpacesTriangle1X5/40_patternPrintSpacesTriangle1X5.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int total_rows;
+
+    cout<<"Enter The Rows: ";
+    cin>>total_rows;
+
+    for (int rows = 1; rows <=total_rows; rows++)
+    {
+        for (int cols= 1; cols<=total_rows-rows; cols++)
+        {
+            cout<<" ";
+        }
+        for (int cols=rows;cols>=1; cols--)
+        {
+            cout<<cols;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
